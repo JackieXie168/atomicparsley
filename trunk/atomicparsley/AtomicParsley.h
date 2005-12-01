5c3319cca98a7c54ca6114a2fe6708c77b9231e9
puck_lock <puck_lock@5439ecd6-7410-0410-b824-b6c1159294a8>
2005-12-01 00:05:43 +0000

    fixes to PrintDataAtoms, RemoveAtom; added '��url', '��inf' & '��ed1' atoms; cli option to remove all artwork

--- trunk/atomicparsley/AtomicParsley.h
+++ trunk/atomicparsley/AtomicParsley.h
@@ -32,6 +32,20 @@ const int AtomicDataClass_JPEGBinary = 13; // \x0D
 const int AtomicDataClass_PNGBinary = 14;  // \x0E
 const int AtomicDataClass_CPIL_TMPO = 21;  // \x15 for cpil, tmpo, rtng, tool; iTMS atoms: cnID, atID, plID, geID, sfID, akID, stik
 
+//////////////////////////////////////////what Quicktime has to say on the the data type subject:
+/* Well-known data type code
+enum {
+  kQTMetaDataTypeBinary         = 0,
+  kQTMetaDataTypeUTF8           = 1,
+  kQTMetaDataTypeUTF16BE        = 2,
+  kQTMetaDataTypeMacEncodedText = 3,
+  kQTMetaDataTypeSignedIntegerBE = 21,  // The size of the integer is defined by the value size
+  kQTMetaDataTypeUnsignedIntegerBE = 22, // The size of the integer is defined by the value size
+  kQTMetaDataTypeFloat32BE      = 23,
+  kQTMetaDataTypeFloat64BE      = 24
+};*/
+////////////////////////////////////////////////
+
 struct AtomicInfo  {
 	short AtomicNumber;
 	long AtomicStart;
@@ -78,15 +92,18 @@ void APar_RemoveAtom(const char* atom_path, bool shellAtom);
 void APar_WriteFile(const char* m4aFile, bool rewrite_original);
 
 //--------------------------------------------------------------------------------------------------------------------------------//
-// v0.1  10/05/2005 Parsing of atoms; intial Tree printout; extraction of all "covr.data" atoms out to files
-// v0.2  11/10/2005 AtomicInfo.NextAtomNumber introduced to facilitate dynamic atom tree reorganization; CreateSparseAtom added
-// v0.5  11/22/2005 Writes artist properly of variable lengths properly into an iTMS m4p file properly (other files don't fare well due to the stsd atom non-standard nature); a number of code-uglifying workarounds were employed to get get that far;
-// v0.6  11/25/2005 Added genre string/numerical support, support for genre's dual-atom ©gen/gnre nature, genre string->integer; bug fixes to APar_LocateAtomInsertionPoint when an atom is missing; APar_CreateSparseAtom for ordinary non-data atoms are now type -1 (which means they aren't of any interest to us besides length & name); implemnted the Integer data class; char4short; verified iTunes standard genres only go up to "Hard Rock"; added jpg/png artwork embedding into "covr" atoms; slight bugfix for APar_FindAtom (created spurious trailing "covr" atoms).
-// v0.6  GPL'ed at sourceforge.net
-// v0.65  11/25/2005  bugfixes to newly introduced bugs in APar_FindAtom; metaEnema to remove all metadata (safe even for m4p drm files); year implemented properly (tagtime moved onto non-standard 'tdtg' atom ala id3v2.4 - because I like that tag); added setting compilation "cpil" tag (an annoying 5byte tag); added advisory setting (maybe it'll give me a kick one cold winter day-do a "Get Info" in iTunes & in the main "Summary" tab view will be a new little icon next to artwork)
-// v0.7  11/26/2005 added a writeBack flag to for a less beta-like future; integrated NSImage resizing of artwor; environmental preferences for artwork modifications; build system mods for Mac-specific compiling; 
-// v0.7.1  11/27/2005 modified parsing & writing to support Apple Lossless (alac) mp4 files. The lovely "alac.alac" non-standard atoms (parents & carry data) caused unplayable files to be written.
-
+/*
+v0.1   10/05/2005 Parsing of atoms; intial Tree printout; extraction of all "covr.data" atoms out to files
+v0.2   11/10/2005 AtomicInfo.NextAtomNumber introduced to facilitate dynamic atom tree reorganization; CreateSparseAtom added
+v0.5   11/22/2005 Writes artist properly of variable lengths properly into an iTMS m4p file properly (other files don't fare well due to the stsd atom non-standard nature); a number of code-uglifying workarounds were employed to get get that far;
+v0.6   11/25/2005 Added genre string/numerical support, support for genre's dual-atom ©gen/gnre nature, genre string->integer; bug fixes to APar_LocateAtomInsertionPoint when an atom is missing; APar_CreateSparseAtom for ordinary non-data atoms are now type -1 (which means they aren't of any interest to us besides length & name); implemnted the Integer data class; char4short; verified iTunes standard genres only go up to "Hard Rock"; added jpg/png artwork embedding into "covr" atoms; slight bugfix for APar_FindAtom (created spurious trailing "covr" atoms).
+v0.6    GPL'ed at sourceforge.net
+v0.65   11/25/2005  bugfixes to newly introduced bugs in APar_FindAtom; metaEnema to remove all metadata (safe even for m4p drm files); year implemented properly (tagtime moved onto non-standard 'tdtg' atom ala id3v2.4 - because I like that tag); added setting compilation "cpil" tag (an annoying 5byte tag); added advisory setting (maybe it'll give me a kick one cold winter day-do a "Get Info" in iTunes & in the main "Summary" tab view will be a new little icon next to artwork)
+v0.7    11/26/2005 added a writeBack flag to for a less beta-like future; integrated NSImage resizing of artwork; environmental preferences for artwork modifications; build system mods for Mac-specific compiling; 
+v0.7.1  11/27/2005 modified parsing & writing to support Apple Lossless (alac) mp4 files. The lovely "alac.alac" non-standard atoms (parents & carry data) caused unplayable files to be written. Only QT ISMA files get screwed now (no idea about Nero)
+v0.7.2  11/29/2005 creates iTunes-required meta.hdlr; all the tags now get spit back when reading them (--textdata); slight fix to how atoms are parsed; all known m4a files now tag properly: iTunes (m4a, m4b, chapterized, alac), Quicktime (ISMA & mpeg4 - change filename ext to .m4a to see art; all QT products require the meta.hdlr addition), faac, Helix Producer & Nero; slight change to how PrintDataAtoms called FindParentAtom; added tag time on "©ed1" (edit date-might only really belong directly under udta); added "©url" to hold url; fixes to APar_RemoveAtom; added cli ability to remove all artwork
+*/
+// goals for 0.8 Switch over to uint8, 16, & 32 to carry data; char got unweildy for non-textual data; short sucked for odd bytes.
 // goals for v0.99 supporting big endian systems
 // goals for 1.x UTF-8 support; perhaps even full blown UTF-16 (unlikely).
 // TODO: revisit how atoms are parsed to get around the tricks for atoms like 'alac', 'drms', 'stsd', 'mp4a' & their writing
