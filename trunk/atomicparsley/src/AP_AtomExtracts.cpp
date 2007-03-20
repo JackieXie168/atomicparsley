682a41985252a84a1f7fe5a5023e7753a01e8ac8
puck_lock <puck_lock@5439ecd6-7410-0410-b824-b6c1159294a8>
2007-03-20 06:32:06 +0000

    add iTunes sorting atoms; Apple TV brand

--- trunk/atomicparsley/src/AP_AtomExtracts.cpp
+++ trunk/atomicparsley/src/AP_AtomExtracts.cpp
@@ -361,37 +361,59 @@ void APar_ShowMPEG4VisualProfileInfo(TrackInfo* track_info) {
 APar_ShowMPEG4AACProfileInfo
 	track_info - a pointer to the struct holding all the information gathered as a single 'trak' atom was traversed
 
-    All (or whatever was in 14496-2-1988) MPEG-4 Audio (0x40, aka 14496-3) are listed; so is mp3 inside the mp4 container, otherwise generic printout. If an
-		'esds' atom was found, then if its section 5 length is known, then 'esds' was carrying channel info (in which case, channels was listed twice, so prefer this
-		listing); otherwise fall back to 'mp4a' carrying channel info (indicated by the parens() around it).
 ----------------------*/
 void APar_ShowMPEG4AACProfileInfo(TrackInfo* track_info) {
 	if (track_info->descriptor_object_typeID == 1) {
 		fprintf(stdout, "  MPEG-4 AAC Main Profile");
 	} else if (track_info->descriptor_object_typeID == 2) {
-		fprintf(stdout, "  MPEG-4 AAC Low Complexity Profile");     //most files will land here
+		fprintf(stdout, "  MPEG-4 AAC Low Complexity/LC Profile");     //most files will land here
 	} else if (track_info->descriptor_object_typeID == 3) {
-		fprintf(stdout, "  MPEG-4 AAC Scaleable Sample Rate Profile");
+		fprintf(stdout, "  MPEG-4 AAC Scaleable Sample Rate/SSR Profile");
 	} else if (track_info->descriptor_object_typeID == 4) {
-		fprintf(stdout, "  MPEG-4 AAC T/F Profile");
+		fprintf(stdout, "  MPEG-4 AAC Long Term Prediction Profile");
 	} else if (track_info->descriptor_object_typeID == 5) {
-		fprintf(stdout, "  MPEG-4 AAC T/F Main Profile");
+		fprintf(stdout, "  MPEG-4 AAC High Efficiency/HE Profile");
 	} else if (track_info->descriptor_object_typeID == 6) {
-		fprintf(stdout, "  MPEG-4 AAC T/F LC Profile");
+		fprintf(stdout, "  MPEG-4 AAC Scalable Profile");
 	} else if (track_info->descriptor_object_typeID == 7) {
-		fprintf(stdout, "  MPEG-4 AAC TwinVQ Profile");
+		fprintf(stdout, "  MPEG-4 AAC Transform domain Weighted INterleave Vector Quantization/TwinVQ Profile");
 	} else if (track_info->descriptor_object_typeID == 8) {
-		fprintf(stdout, "  MPEG-4 AAC CELP Profile");
+		fprintf(stdout, "  MPEG-4 AAC Code Excited Linear Predictive/CELP Profile");
 	} else if (track_info->descriptor_object_typeID == 9) {
 		fprintf(stdout, "  MPEG-4 AAC HVXC Profile");
-	} else if (track_info->descriptor_object_typeID == 10) {
-		fprintf(stdout, "  MPEG-4 AAC HILN Profile");
-	} else if (track_info->descriptor_object_typeID == 11) {
-		fprintf(stdout, "  MPEG-4 AAC TTSI Profile");
+
 	} else if (track_info->descriptor_object_typeID == 12) {
-		fprintf(stdout, "  MPEG-4 AAC Main Synthesis Profile");
+		fprintf(stdout, "  MPEG-4 AAC TTSI Profile");
 	} else if (track_info->descriptor_object_typeID == 13) {
+		fprintf(stdout, "  MPEG-4 AAC Main Synthesis Profile");
+	} else if (track_info->descriptor_object_typeID == 14) {
 		fprintf(stdout, "  MPEG-4 AAC Wavetable Synthesis Profile");
+	} else if (track_info->descriptor_object_typeID == 15) {
+		fprintf(stdout, "  MPEG-4 AAC General MIDI Profile");
+	} else if (track_info->descriptor_object_typeID == 16) {
+		fprintf(stdout, "  MPEG-4 AAC Algorithmic Synthesis & Audio FX Profile");
+	} else if (track_info->descriptor_object_typeID == 17) {
+		fprintf(stdout, "  MPEG-4 AAC AAC Low Complexity/LC (+error recovery) Profile");
+	
+	} else if (track_info->descriptor_object_typeID == 19) {
+		fprintf(stdout, "  MPEG-4 AAC Long Term Prediction (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 20) {
+		fprintf(stdout, "  MPEG-4 AAC Scalable (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 21) {
+		fprintf(stdout, "  MPEG-4 AAC Transform domain Weighted INterleave Vector Quantization/TwinVQ (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 22) {
+		fprintf(stdout, "  MPEG-4 AAC Bit Sliced Arithmetic Coding/BSAC (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 23) {
+		fprintf(stdout, "  MPEG-4 AAC Low Delay/LD (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 24) {
+		fprintf(stdout, "  MPEG-4 AAC Code Excited Linear Predictive/CELP (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 25) {
+		fprintf(stdout, "  MPEG-4 AAC HXVC (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 26) {
+		fprintf(stdout, "  MPEG-4 AAC Harmonic and Individual Lines plus Noise/HILN (+error recovery) Profile");
+	} else if (track_info->descriptor_object_typeID == 27) {
+		fprintf(stdout, "  MPEG-4 AAC Parametric (+error recovery) Profile");
+		
 	} else if (track_info->descriptor_object_typeID == 31) {
 		fprintf(stdout, "  MPEG-4 ALS Audio Lossless Coding"); //I think that mp4alsRM18 writes the channels wrong after objectedID: 0xF880 has 0 channels; 0xF890 is 2ch
 	} else {
